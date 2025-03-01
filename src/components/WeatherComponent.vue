aef4ca527aade31c55425b620fbe6bc8677d41d7
hardiksavani35 <hardiksavaniaus@gmail.com>
2025-03-02 00:20:23 +1030

    - Filter

--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -1,6 +1,7 @@
 <template>
-    <div> 
-        <div v-for="city in cities" :key="city"> 
+    <div>
+        <weather-filter @search-city="filterCities"></weather-filter>
+        <div v-for="city in filteredCities" :key="city"> 
             <weather-box :city="city"></weather-box>
         </div>
     </div>
@@ -8,13 +9,31 @@
 
 <script>
 import WeatherBox from './WeatherBox.vue';
+import WeatherFilter from './WeatherFilter.vue';
 
 export default {
     data() {
         return {
+            searchQuery: "",
             cities: ["adelaide", "sydney", "melbourne", "brisbane", "perth"]
         }
     },
-    components: {WeatherBox}
+    components: {
+        WeatherBox, 
+        WeatherFilter
+    },
+    computed: {
+        filteredCities() {
+            if (!this.searchQuery) {
+                return this.cities;  
+            }
+            return this.cities.filter(city => city.toLowerCase().includes(this.searchQuery));
+        }
+    },
+    methods: {
+        filterCities(query) {
+            this.searchQuery = query;  
+        }
+    },
 }
 </script>
---
+++ src/components/WeatherFilter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input type="text" v-model="search" @input="searchData" placeholder="Search city" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            search: ''
+        }
+    }, 
+    methods: {
+        searchData() {
+            this.$emit('search-city', this.search.toLowerCase());
+        }
+    }
+}
+</script>
+
+<style scope>
+    input {    
+        width: 100%;
+        padding: 10px;
+        margin: 10px 0px;
+        border-radius: 8px;
+        border: 1px solid #c6b1b1;
+    }
+</style>
